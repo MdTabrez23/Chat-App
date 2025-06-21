55e9b2b30a2591b0f3cebd7358fb293fcc36127f
Md Tabrez <mohammad.tabrez236@gmail.com>
2025-06-22 03:40:10 +0530

    SIGNUP BACKEND COMEPLETED

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,17 +1,21 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "Fill all the credentials." });
+    }
     if (password.length < 8) {
       return res
         .status(400)
-        .jspn({ message: "Password must be at least 8 characters" });
+        .json({ message: "Password must be at least 8 characters" });
     }
     const user = await User.findOne({ email });
     if (user) return res.status(400).json({ message: "Email already exists." });
-    const salat = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({
       fullName,
@@ -21,12 +25,25 @@ export const signup = async (req, res) => {
 
     if (newUser) {
       //jwt token
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
     } else {
       res.status(400).json({
         message: "Invalid user data",
       });
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log("Error in signup controller: ", error.message);
+    res.status(400).json({
+      message: "Internal server error.",
+    });
+  }
 };
 
 export const login = (req, res) => {
